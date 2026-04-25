9cdb464297c9d31daab8b9ccb4db7390ed34c8f3
ykaankaradag <yusufkaankaradag@gmail.com>
2026-04-25 15:50:05 +0300

    Add files via upload

---
+++ Sliding Tiles - v1.1/sprite.py
@@ -0,0 +1,71 @@
+import pygame
+from ayarlar import * 
+
+pygame.font.init()
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, game, x, y, text):
+        self.groups = game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pygame.Surface((KARE, KARE))
+        self.x, self.y = x, y
+        self.text = text
+        self.rect = self.image.get_rect()
+        if self.text != "empty":
+            self.font = pygame.font.SysFont("Consolas", 50)
+            font_surface = self.font.render(self.text, True, BLACK)
+            self.image.fill(WHITE)
+            self.font_size = self.font.size(self.text)
+            draw_x =(KARE / 2) - self.font_size[0] / 2
+            draw_y =(KARE / 2) - self.font_size[1] / 2
+            self.image.blit(font_surface, (draw_x, draw_y))
+
+    def update(self):
+        self.rect.x = self.x * KARE
+        self.rect.y = self.y * KARE
+
+    def click(self, mouse_x, mouse_y):
+        return self.rect.left <= mouse_x <= self.rect.right and self.rect.top <= mouse_y <= self.rect.bottom
+    
+    def right(self):
+        return self.rect.x + KARE < OYUN_BOYUTU * KARE        
+    def left(self):
+        return self.rect.x - KARE >= 0
+    def up(self):
+        return self.rect.y - KARE >= 0
+    def down(self):
+        return self.rect.y + KARE < OYUN_BOYUTU * KARE
+class Menu:
+    def __init__(self, x, y, text):
+        self.x, self.y = x, y
+        self.text = text
+    
+    def draw(self, screen):
+        font = pygame.font.SysFont("Consolas", 30)
+        text = font.render(self.text, True, WHITE)
+        screen.blit(text, (self.x, self.y))
+
+class Button:
+    def __init__(self, x, y, width, height, text, renk, text_rengi):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.renk = renk
+        self.text_rengi = text_rengi
+
+        self.font = pygame.font.SysFont("Consolas", 30)
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.renk, (self.x, self.y, self.width, self.height) )
+        font = pygame.font.SysFont("Consolas", 30)
+        text = font.render(self.text, True, self.text_rengi)
+        self.font_size = self.font.size(self.text)
+        draw_x = self.x + (self.width / 2) - self.font_size[0] / 2
+        draw_y = self.y + (self.height / 2) - self.font_size[1] / 2
+        screen.blit(text, (draw_x, draw_y))
+
+    def click(self, mouse_x, mouse_y):
+        return self.x<= mouse_x <= self.x + self.width and self.y <= mouse_y <= self.y + self.height
